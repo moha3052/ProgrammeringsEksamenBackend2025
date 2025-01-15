5f30a54099ff11c1a2ca55f93df779ba411d2680
moha3052 <moha3052@hotmail.dk>
2025-01-15 20:24:37 +0100

    Eksamensprojekt 2025

---
+++ src/main/java/com/example/programmeringseksamenbackend2025/ProgrammeringsEksamenBackend2025Application.java
@@ -0,0 +1,13 @@
+package com.example.programmeringseksamenbackend2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammeringsEksamenBackend2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammeringsEksamenBackend2025Application.class, args);
+    }
+
+}
