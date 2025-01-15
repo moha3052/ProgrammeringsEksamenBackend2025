4aafdb1b1b25a12f4e75ac73fbecd68b1af4175c
moha3052 <moha3052@hotmail.dk>
2025-01-15 20:59:42 +0100

    Environment værdier

--- src/main/java/com/example/programmeringseksamenbackend2025/ProgrammeringsEksamenBackend2025Application.java
+++ src/main/java/com/example/programmeringseksamenbackend2025/ProgrammeringsEksamenBackend2025Application.java
@@ -8,9 +8,6 @@ public class ProgrammeringsEksamenBackend2025Application {
 
     public static void main(String[] args) {
         SpringApplication.run(ProgrammeringsEksamenBackend2025Application.class, args);
-
-
-
     }
 
 }
